d415550508609a7cf7c330a95c53b1666e283433
kerembay9 <kerembay9@gmail.com>
2024-05-07 09:11:25 +0300

    added go native for click action

--- main.js
+++ main.js
@@ -1,131 +1,145 @@
 const {
-    app,
-    BrowserWindow,
-    desktopCapturer,
-    ipcMain,
-    Menu,
-    screen,
-} = require('electron');
-const path = require('path');
+  app,
+  BrowserWindow,
+  desktopCapturer,
+  ipcMain,
+  Menu,
+  screen,
+} = require("electron");
+const { spawn } = require("child_process");
+const path = require("path");
 // const robot = require('robotjs');
 const { io } = require("socket.io-client");
-const socket = io.connect('wss://hiverdp.sharencare.com.tr/', { transports: ['websocket'] });
+const socket = io.connect("wss://hiverdp.sharencare.com.tr/", {
+  transports: ["websocket"],
+});
 
-socket.on('mouse_click', (coordinates) => {
-    console.log('coordinates are: ', coordinates)
-    // handleMouseDown(coordinates)
-  })
-app.commandLine.appendSwitch ("disable-http-cache");
+socket.on("mouse_click", (coordinates) => {
+  console.log("coordinates are: ", coordinates);
+  handleMouseDown(coordinates)
+});
+app.commandLine.appendSwitch("disable-http-cache");
 let lastClickTime = new Date().getTime();
-let availableScreens
-let mainWindow
-let displays
+let availableScreens;
+let mainWindow;
+let displays;
 
 const sendSelectedScreen = (item) => {
-    console.log('setting source id to: ', item.id)
-    const mainScreen = screen.getPrimaryDisplay();
-    const displaySize = mainScreen.size;
-    mainWindow.webContents.send('SET_SOURCE_ID', {
-        id: item.id,
-        dimension: displaySize,
-    })
-    socket.emit('source_dim',displaySize);
-}
+  console.log("setting source id to: ", item.id);
+  const mainScreen = screen.getPrimaryDisplay();
+  const displaySize = mainScreen.size;
+  mainWindow.webContents.send("SET_SOURCE_ID", {
+    id: item.id,
+    dimension: displaySize,
+  });
+  socket.emit("source_dim", displaySize);
+};
 const createTray = () => {
-    const screensMenu = availableScreens.map(item => {
-        return {
-            label: item.name,
-            click: () => {
-                sendSelectedScreen(item)
-            }
-        }
-    })
-    const menu = Menu.buildFromTemplate([
-        {
-            label: app.name,
-            submenu: [
-                { role: 'quit' }
-            ]
-        },
-        // {
-        //     label: 'Screens',
-        //     submenu: screensMenu
-        // }
-    ])
+  const screensMenu = availableScreens.map((item) => {
+    return {
+      label: item.name,
+      click: () => {
+        sendSelectedScreen(item);
+      },
+    };
+  });
+  const menu = Menu.buildFromTemplate([
+    {
+      label: app.name,
+      submenu: [{ role: "quit" }],
+    },
+    // {
+    //     label: 'Screens',
+    //     submenu: screensMenu
+    // }
+  ]);
 
-    Menu.setApplicationMenu(menu)
-}
+  Menu.setApplicationMenu(menu);
+};
 const createWindow = () => {
-    mainWindow = new BrowserWindow({
-        show: false,
-        width: 800,
-        height: 600,
-        fullscreen: true,
-        webPreferences: {
-            nodeIntegration: false,
-            contextIsolation: true,
-            preload: path.join(__dirname, 'preload.js'),
-                }
-    })
-    ipcMain.on('GET_SOURCE_ID', (event) => {
-        if (availableScreens){
-            const mainScreen = screen.getPrimaryDisplay();
-            const displaySize = mainScreen.size;
-            event.reply('SET_SOURCE_ID', {
-            id: availableScreens[0],
-            dimension: displaySize,
-            });
-      }});
-    ipcMain.on('JOIN_ROOM', (_event,room_id) => {
-        socket.emit('join',room_id)
+  mainWindow = new BrowserWindow({
+    show: false,
+    width: 800,
+    height: 600,
+    fullscreen: true,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+  ipcMain.on("GET_SOURCE_ID", (event) => {
+    if (availableScreens) {
+      const mainScreen = screen.getPrimaryDisplay();
+      const displaySize = mainScreen.size;
+      event.reply("SET_SOURCE_ID", {
+        id: availableScreens[0],
+        dimension: displaySize,
+      });
+    }
+  });
+  ipcMain.on("JOIN_ROOM", (_event, room_id) => {
+    socket.emit("join", room_id);
+  });
+  ipcMain.on("set-size", (event, size) => {
+    const { width, height } = size;
+    try {
+      console.log("electron dim..", width, height);
+      !isNaN(height) && mainWindow.setSize(width, height, false);
+    } catch (e) {
+      console.log(e);
+    }
+  });
+  // TO-DO: load proper url
+  mainWindow.loadURL("https://www.aygen.hm-rdp.com/login", {
+    extraHeaders: "pragma: no-cache\n",
+  });
+    // mainWindow.loadURL("http://localhost:3000/login", {
+    //     extraHeaders: "pragma: no-cache\n",
+    // });
+  mainWindow.once("ready-to-show", () => {
+    displays = screen.getAllDisplays();
+
+    mainWindow.show();
+    mainWindow.setPosition(0, 0);
+
+    desktopCapturer
+      .getSources({
+        types: ["screen"],
+      })
+      .then((sources) => {
+        sendSelectedScreen(sources[0]);
+        availableScreens = sources;
+        createTray();
+      });
+  });
+  mainWindow.webContents.openDevTools();
+};
+app.on("ready", () => {
+  createWindow();
+  console.log("hi");
+});
+
+async function handleMouseDown(coordinates) {
+  const currentTime = new Date().getTime();
+  const timeSinceLastClick = currentTime - lastClickTime;
+  if (timeSinceLastClick > 0 && timeSinceLastClick < 300) {
+    console.log("Double click detected!");
+    const go = spawn("./click_go", [Math.round(coordinates.x), Math.round(coordinates.y), true]);
+    go.stdout.on("data", (data) => {
+      console.log(`stdout: ${data}`);
     });
-    ipcMain.on('set-size', (event, size) => {
-        const { width, height } = size
-        try {
-            console.log('electron dim..', width, height)
-            !isNaN(height) && mainWindow.setSize(width, height, false)
-        } catch (e) {
-            console.log(e)
-        }
-    })           
-        // TO-DO: load proper url
-        mainWindow.loadURL('http://localhost:3000/electron', {"extraHeaders" : "pragma: no-cache\n"})
-        mainWindow.once('ready-to-show', () => { 
-        displays = screen.getAllDisplays()
-
-        mainWindow.show()
-        mainWindow.setPosition(0, 0)
-
-        desktopCapturer.getSources({
-            types: ['screen']
-        }).then(sources => {
-            sendSelectedScreen(sources[0])
-            availableScreens = sources
-            createTray()
-        })
-    })
-    mainWindow.webContents.openDevTools()
+    go.stderr.on("data", (data) => {
+      console.error(`stderr: ${data}`);
+    });
+  } else {
+    const go = spawn("./click_go", [Math.round(coordinates.x), Math.round(coordinates.y), false]);
+    go.stdout.on("data", (data) => {
+      console.log(`stdout: ${data}`);
+    });
+    go.stderr.on("data", (data) => {
+      console.error(`stderr: ${data}`);
+    });
+  }
+  lastClickTime = currentTime;
 }
-app.on('ready', () => {
-    createWindow()
-    console.log('hi')
-})
-  
-
-
-// async function handleMouseDown(coordinates) {
-//     const currentTime = new Date().getTime();
-//     const timeSinceLastClick = currentTime - lastClickTime;
-//     if (timeSinceLastClick > 0 && timeSinceLastClick < 300) {
-//       console.log('Double click detected!');
-//       await robot.moveMouse(coordinates.x, coordinates.y);
-//       robot.mouseClick('left', true)
-//     } else {
-//       console.log('Single click detected!');
-//       await robot.moveMouse(coordinates.x, coordinates.y);
-//       robot.mouseClick('left', false)
-//     }
-//     lastClickTime = currentTime;
-//   }
-
- 
